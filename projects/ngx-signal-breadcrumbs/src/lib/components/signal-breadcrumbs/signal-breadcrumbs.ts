04cee0bad3ff2a1d8f58ffb6bebdddfdbb951a36
CarlesRa <jcramos8282@gmail.com>
2026-01-31 19:03:40 +0100

    change npm version

--- projects/ngx-signal-breadcrumbs/src/lib/components/signal-breadcrumbs/signal-breadcrumbs.ts
+++ projects/ngx-signal-breadcrumbs/src/lib/components/signal-breadcrumbs/signal-breadcrumbs.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BreadcrumbService } from '../../services';
 import { SiIcon } from '../si-icon/si-icon';
@@ -10,13 +10,14 @@ import { IconName } from '../../constants';
   imports: [RouterLink, SiIcon],
   templateUrl: './signal-breadcrumbs.html',
   styleUrl: './signal-breadcrumbs.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignalBreadcrumbs {
   showHome = input<boolean>(true);
   linkColor = input<string>('#3b82f6');
   linkHoverColor = input<string>('#93c5fd');
   separatorColor = input<string>('#94a3b8');
-  separator = input<IconName>('chevron-right' as IconName)
+  separator = input<IconName>('chevron-right')
 
   private breadcrumbService = inject(BreadcrumbService);
   protected breadcrumbs = this.breadcrumbService.breadcrumbs
